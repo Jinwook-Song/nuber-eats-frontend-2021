c1b130ee036a71c550838a4b76c999278acf3dee
jinwook-song <wlsdnr129@naver.com>
2021-05-24 02:10:51 +0900

    lecture 15.0 Local Only Fields

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+
+// reactive variables
+export const isLoggedInVar = makeVar(false);
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    // save local state into cache
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar();
+            },
+          },
+        },
+      },
+    },
+  }),
 });

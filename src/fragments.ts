e1c51fa90d4de5daa99c1a3b185efa6258c63dde
jinwook-song <wlsdnr129@naver.com>
2021-06-01 18:43:10 +0900

    lecture 17.6 Category

--- src/fragments.ts
+++ src/fragments.ts
@@ -12,3 +12,13 @@ export const RESTAURANT_FRAGMENT = gql`
     address
   }
 `;
+
+export const CATEGORY_FRAGMENT = gql`
+  fragment CategoryParts on Category {
+    id
+    name
+    coverImg
+    slug
+    restaurantCount
+  }
+`;

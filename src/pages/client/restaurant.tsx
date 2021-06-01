d4a51a51ca1c6209b00fd626656b44fe5f31b0bf
jinwook-song <wlsdnr129@naver.com>
2021-06-01 19:38:56 +0900

    lecture 17.8~17.9 Restaurant UI

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,141 +1,64 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import { Helmet } from "react-helmet-async";
-import { gql, useQuery } from "@apollo/client";
-import { url } from "inspector";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useHistory } from "react-router";
-import { Restaurant } from "../../components/restaurant";
+import { useParams } from "react-router";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
 import {
-  restaurantsPageQuery,
-  restaurantsPageQueryVariables,
-} from "../../__generated__/restaurantsPageQuery";
-import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
-import { Link } from "react-router-dom";
+  restaurant,
+  restaurantVariables,
+} from "../../__generated__/restaurant";
 
-const RESTAURANTS_QUERY = gql`
-  query restaurantsPageQuery($input: RestaurantsInput!) {
-    allCategories {
-      ok
+const RESTAURANT_QUERY = gql`
+  query restaurant($input: RestaurantInput!) {
+    restaurant(input: $input) {
       error
-      categories {
-        ...CategoryParts
-      }
-    }
-    restaurants(input: $input) {
       ok
-      error
-      totalPages
-      totalResults
-      results {
+      restaurant {
         ...RestaurantParts
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
-  ${CATEGORY_FRAGMENT}
 `;
 
-interface IFormProps {
-  searchTerm: string;
+interface IRestaurantParams {
+  id: string;
 }
-
-export const Restaurants = () => {
-  const [page, setPage] = useState(1);
-  const { data, loading } = useQuery<
-    restaurantsPageQuery,
-    restaurantsPageQueryVariables
-  >(RESTAURANTS_QUERY, {
-    variables: {
-      input: {
-        page,
+export const Restaurant = () => {
+  const params = useParams<IRestaurantParams>();
+  const { loading, data } = useQuery<restaurant, restaurantVariables>(
+    RESTAURANT_QUERY,
+    {
+      variables: {
+        input: {
+          restaurantId: +params.id,
+        },
       },
-    },
-  });
-  const onNextPageClick = () => setPage((current) => current + 1);
-  const onPrevPageClick = () => setPage((current) => current - 1);
-  const { register, handleSubmit, getValues } = useForm<IFormProps>();
-  const history = useHistory();
-  const onSearchSubmit = () => {
-    const { searchTerm } = getValues();
-    history.push({
-      pathname: "/search",
-      search: `?term=${searchTerm}`,
-    });
-  };
+    }
+  );
   return (
     <div>
       <Helmet>
-        <title>Home | Nuber Eats</title>
+        <title>{data?.restaurant.restaurant?.name || ""} | Nuber Eats</title>
       </Helmet>
-      <form
-        onSubmit={handleSubmit(onSearchSubmit)}
-        className="bg-gray-800 w-full py-40 flex items-center justify-center"
+      <div
+        className=" bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
       >
-        <input
-          {...register("searchTerm", {
-            required: true,
-            minLength: 3,
-          })}
-          type="Search"
-          className="input rounded-md border-0 w-9/12 md:w-3/12"
-          placeholder="Search restaurants..."
-        />
-      </form>
-      {!loading && (
-        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
-          <div className="flex justify-around max-w-sm mx-auto ">
-            {data?.allCategories.categories?.map((category) => (
-              <Link key={category.id} to={`/category/${category.slug}`}>
-                <div className="flex flex-col group items-center cursor-pointer">
-                  <div
-                    className=" w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
-                    style={{ backgroundImage: `url(${category.coverImg})` }}
-                  ></div>
-                  <span className="mt-1 text-sm text-center font-medium">
-                    {category.name}
-                  </span>
-                </div>
-              </Link>
-            ))}
-          </div>
-          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
-            {data?.restaurants.results?.map((restaurant) => (
-              <Restaurant
-                key={restaurant.id}
-                id={restaurant.id + ""}
-                coverImg={restaurant.coverImg}
-                name={restaurant.name}
-                categoryName={restaurant.category?.name}
-              />
-            ))}
-          </div>
-          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
-            {page > 1 ? (
-              <button
-                onClick={onPrevPageClick}
-                className="focus:outline-none font-medium text-xl"
-              >
-                &larr;
-              </button>
-            ) : (
-              <div></div>
-            )}
-            <span>
-              Page {page} of {data?.restaurants.totalPages}
-            </span>
-            {page !== data?.restaurants.totalPages ? (
-              <button
-                onClick={onNextPageClick}
-                className="focus:outline-none font-medium text-xl"
-              >
-                &rarr;
-              </button>
-            ) : (
-              <div></div>
-            )}
-          </div>
+        <div className="bg-white w-3/12 py-8 pl-10 xl:pl-48">
+          <h4 className="text-4xl mb-3 text">
+            {data?.restaurant.restaurant?.name}
+          </h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -3,7 +3,8 @@ import { Header } from "../components/header";
 import { useMyProfile } from "../hooks/useMyProfile";
 import { NotFound } from "../pages/404";
 import { Category } from "../pages/client/category";
-import { Restaurants } from "../pages/client/restaurant";
+import { Restaurant } from "../pages/client/restaurant";
+import { Restaurants } from "../pages/client/restaurants";
 import { Search } from "../pages/client/search";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
@@ -24,6 +25,9 @@ const ClientRoutes = [
   </Route>,
   <Route key={5} path="/category/:slug">
     <Category />
+  </Route>,
+  <Route key={6} path="/restaurants/:id">
+    <Restaurant />
   </Route>,
 ];
 

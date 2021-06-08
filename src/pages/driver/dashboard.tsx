4d0a0aaeab5823f652f712984607ef55353af396
jinwook-song <wlsdnr129@naver.com>
2021-06-08 20:08:52 +0900

    lecture 23.6 Driver Dashboard part 2

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,6 +1,29 @@
 import GoogleMapReact from "google-map-react";
+import { useEffect, useState } from "react";
+
+interface ICoords {
+  lat: number;
+  lng: number;
+}
 
 export const Dashboard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
+  const onSuccess = ({
+    coords: { latitude, longitude },
+  }: GeolocationPosition) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+  const onError = (error: GeolocationPositionError) => {
+    console.log(error);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+  };
   return (
     <div>
       <div
@@ -8,12 +31,12 @@ export const Dashboard = () => {
         style={{ width: window.innerWidth, height: "95vh" }}
       >
         <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
           defaultZoom={20}
-          defaultCenter={{ lat: 59.95, lng: 30.33 }}
+          defaultCenter={{ lat: 36.56, lng: 127.04 }}
           bootstrapURLKeys={{ key: "AIzaSyACY9PPICeYtG6F9VR_XWKDPtWbAhiOnA4" }}
-        >
-          <h1>Hello</h1>
-        </GoogleMapReact>
+        ></GoogleMapReact>
       </div>
     </div>
   );
